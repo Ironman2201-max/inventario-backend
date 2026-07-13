90435489846c9072e6f1e17b9941efca7fdf65ad
Ironman2201-max <jose113292322@gmail.com>
2026-07-12 19:54:03 -0500

    Update CORS settings and database connection details

--- conexion.php
+++ conexion.php
@@ -1,22 +1,22 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: https://sgp.seminario1.eleueleo.com");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Content-Type: application/json; charset=UTF-8");
 
-// Manejar la petición PREFLIGHT de CORS (OPTIONS)
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
 
+// Datos de MySQL en el VPS
 $host = "localhost";
-$db_name = "angular_auth_db";
-$username = "root";
-$password = "";
+$db_name = "semi1_sgp_prod";
+$username = "semi1_sgp";
+$password = '$3m1nar10Sgp'; 
 
 try {
-    $pdo = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $password);
+    $pdo = new PDO("mysql:host=" . $host . ";dbname=" . $db_name . ";charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $exception) {
     echo json_encode(["error" => "Error de conexión: " . $exception->getMessage()]);
